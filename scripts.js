e1ce0e22ec558413b58c8f42475a0346ff70acda
CynaneShay <117528257+CynaneShay@users.noreply.github.com>
2023-03-24 12:15:01 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -22,11 +22,3 @@ function bubbleup(x) {
 function bubbledown(x) {
   x.style.background="rgba(255,255,255,1)"; x.style.color="rgba(0,0,0,1)";
 }
-
-function MakeBig(x) {
- x.style.font-size="4.2vw"; 
-}
-
-function MakeLittle(x) {
-   x.style.font-size="4.0vw"; 
-}
