35fe7aa08a8d84358a47ff6dac4b836290532a8d
CynaneShay <117528257+CynaneShay@users.noreply.github.com>
2023-03-28 20:52:48 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,4 +1,16 @@
 let mybutton = document.getElementById("myBtn");
+let slider=tns({container: ".my-slider",
+               "SlideBy": 1,
+               "speed": 400,
+               "nav": false,
+               controlsContainer: "#controls",
+               prevButton: ".previous",
+               nextButton: ".next",
+               responsive: {1600: {items: 5, gutter: 20},
+                           1024: {items: 4, gutter: 20},
+                           768: {items: 3, gutter: 20},
+                           480: {items: 2, gutter: 20}}
+               });
 
 window.onscroll = function() {scrollFunction()};
 
