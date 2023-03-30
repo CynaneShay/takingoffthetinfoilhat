4767008d2782e1b4831e1e682c6440063106dfe3
CynaneShay <117528257+CynaneShay@users.noreply.github.com>
2023-03-29 20:06:42 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -7,9 +7,9 @@ let slider=tns({container: ".my-slider",
                prevButton: ".previous",
                nextButton: ".next",
                responsive: {1600: {items: 5, gutter: 20},
-                           1024: {items: 4, gutter: 20},
-                           768: {items: 3, gutter: 20},
-                           480: {items: 2, gutter: 20}}
+                           1024: {items: 5, gutter: 20},
+                           768: {items: 4, gutter: 20},
+                           480: {items: 3, gutter: 20}}
                });
 
 window.onscroll = function() {scrollFunction()};
