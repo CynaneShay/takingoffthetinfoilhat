89ffae5d08f57c475d2dde7c7b6e2e80b30ae158
CynaneShay <117528257+CynaneShay@users.noreply.github.com>
2023-04-01 17:58:30 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -46,7 +46,7 @@ function makelittle(x) {
 }
 
 function carouselin(x) {
-  x.style.boxShadow= "0 0 0 200px rgba(255,255,255,.9) inset";
+  x.querySelector('.slideimg').style.boxShadow= "0 0 0 200px rgba(255,255,255,.9) inset";
   x.querySelector('.fa-circle-info').style.opacity= "1"; 
   x.querySelector('.fa-circle-info').style.transform= "scale(1)";
   x.querySelector('.fa-cart-shopping').style.opacity= "1"; 
@@ -54,7 +54,7 @@ function carouselin(x) {
 }
 
 function carouselout(x) {
-  x.style.boxShadow= "none";
+  x.querySelector('.slideimg').style.boxShadow= "none";
   x.querySelector('.fa-circle-info').style.opacity= "0"; 
   x.querySelector('.fa-circle-info').style.transform= "scale(0)";
   x.querySelector('.fa-cart-shopping').style.opacity= "0"; 
