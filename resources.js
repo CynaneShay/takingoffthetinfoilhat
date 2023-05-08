e70cb637a2208358fea9f87eafbf2fea567e2012
CynaneShay <117528257+CynaneShay@users.noreply.github.com>
2023-05-08 17:00:53 -0400

    Update resources.js

--- resources.js
+++ resources.js
@@ -116,7 +116,7 @@ let slider=tns({container: ".my-slider",
                controlsContainer: "#controls",
                prevButton: ".previous",
                nextButton: ".next",
-               responsive: {1600: {items: 3, gutter: 5},
+               responsive: {1600: {items: 4, gutter: 5},
                            1024: {items: 3, gutter: 5},
                            768: {items: 3, gutter: 5},
                            480: {items: 2, gutter: 5}}
